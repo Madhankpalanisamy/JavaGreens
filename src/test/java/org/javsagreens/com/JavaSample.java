8a7e36254ca6cf6239a77d2ad73a20c9f04ed07d
madhankpalanisamy <madhankpalanisamy@gmaail.com>
2022-02-28 15:15:34 +0530

    commiting from branch1

--- src/test/java/org/javsagreens/com/JavaSample.java
+++ src/test/java/org/javsagreens/com/JavaSample.java
@@ -2,7 +2,18 @@ package org.javsagreens.com;
 
 public class JavaSample {
 	
+	
+	public void gitbranch() {
+		
+		System.out.println("git branching");
+		System.out.println("Branch1");
+
+	}
+	
 	public static void main(String[] args) {
+		
+		JavaSample obj = new JavaSample();
+		obj.gitbranch();
 		
 	}
 	
